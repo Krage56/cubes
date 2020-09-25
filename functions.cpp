250655c8146e1f6ab7f1f3b32ce02c618f1194fe
Krage56 <ivan.proskuryakov.2001@yandex.ru>
2020-09-25 23:22:01 +0300

    Supposedly finished version

--- functions.cpp
+++ functions.cpp
@@ -23,4 +23,42 @@ tuple<vector<set<char>>, string> processing(ifstream *file) {
     }
     return make_tuple(cubes, word);
 }
-
+vector<long long> getPath(vector<long long>& prior, long long dest, long long origin)
+{
+    vector<long long> ans;
+    if(prior[dest] == -1){
+        return ans;
+    }
+    for (int v = dest; v != origin; v = prior[v])  // Проходим по пути из конца в начало
+    {
+        ans.push_back(v);  // Запоминаем вершину
+    }
+    ans.push_back(origin);
+    reverse(ans.begin(), ans.end());  // Переворачиваем путь
+    return ans;
+}
+vector<long long> BFS(vector<vector<long long>>&_data, long long origin, long long dest){
+    queue<long long> queue;
+    queue.push(origin);
+    vector<bool> visited(_data.size());
+    vector<long long>prior(_data.size());
+    for (long long i = 0; i < _data.size(); ++i) {
+        prior[i] = -1;
+    }
+    long long current = origin;
+    prior[origin] = origin;
+    while (!queue.empty() && current != dest) {
+        current = queue.front();
+        queue.pop();
+        if (!visited[current]) {
+            visited[current] = true;
+            for(long i = 0; i < _data.size(); ++i){
+                if(_data[current][i] != 0){
+                    queue.push(i);
+                    prior[i] = !visited[i] ? current : prior[i];
+                }
+            }
+        }
+    }
+    return prior;
+}
--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@ using namespace std;
 int main() {
     ifstream file;
     ofstream answer;
-    file.open("../cubesTests/test1.in", ios_base::in);
+    file.open("../cubesTests/test3.in", ios_base::in);
     vector<set<char>>cubes; string word;
     tie(cubes, word) = processing(&file);
-    vector<vector<int>>c(2 + cubes.size() + word.size()), f(2 + cubes.size() + word.size());
+    vector<vector<int>> f(2 + cubes.size() + word.size());
+    vector<vector<long long int>> c(2 + cubes.size() + word.size());
     for(long long i = 0; i < f.size(); ++i){
         for (long long j = 0; j < f.size(); ++j){
             f[i].push_back(0);
@@ -25,7 +26,37 @@ int main() {
             c[word.size() + 2 + j][1] = 1;
         }
     }
-
+    vector<long long> prior = move(BFS(c, 0, 1));
+    vector<long long> way = move(getPath(prior, 1, 0));
+    while (!way.empty()){
+        long long prev_point = 0, min_flow = 0;
+        for(const auto el : way){
+            if(c[prev_point][el] < min_flow || min_flow == 0){
+                min_flow = c[prev_point][el];
+            }
+            prev_point = el;
+        }
+        for(int i = 0; i < way.size() - 1; ++i){
+            long long a = way[i], b = way[i+1];
+            f[a][b] += min_flow;
+            f[b][a] -= min_flow;
+            c[a][b] -= min_flow;
+            c[b][a] += min_flow;
+        }
+        prior = move(BFS(c, 0, 1));
+        way = move(getPath(prior, 1, 0));
+        min_flow = 0;
+    }
+    long long res = 0;
+    for (const auto e: f[0]){
+        res += e;
+    }
+    if(res == word.size()){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
     file.close();
     return 0;
 }
